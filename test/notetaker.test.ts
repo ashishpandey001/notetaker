481a21eb28f741452bbed6553344ab383f950b97
Ashishkumar Pandey <ashishpandeyone@gmail.com>
2024-05-06 16:37:07 +0530

    Initial commit

---
+++ test/notetaker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Notetaker from '../lib/notetaker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/notetaker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Notetaker.NotetakerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
